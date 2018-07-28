761a8ea588fe11a70cfad354c619737926ad9d93
dysonance <jamos125@gmail.com>
2018-07-28 00:37:29 -0400

    more hurst exponent bugfixes

--- src/chaos.jl
+++ src/chaos.jl
@@ -2,6 +2,10 @@
 Functionality generally related to chaos theory and dynamical systems.
 =#
 
+
+ptp(x::AbstractVector{Float64}) = maximum(x) - minimum(x)
+
+
 @doc """
 # Rescaled Range
 
@@ -9,8 +13,10 @@ Compute the rescaled range of a time series.
 
 `rescaled_range(x::Vector{Float64})::Float64`
 """ ->
-function rescaled_range(X::Vector{Float64}; corrected::Bool=false)::Float64
-    return (maximum(X) - minimum(X)) / std(X, corrected=false)
+function rescaled_range(X::Vector{Float64}, corrected::Bool=true)
+    r = ptp(cumsum(X - mean(X)))
+    s = std(X, corrected=corrected)
+    return r/s
 end
 
 
@@ -23,7 +29,6 @@ Estimate the Hurst exponent of a time series.
 * `x`: vector of time series data
 * `t`: vector/range of values to use for `t` when estimating the exponent
 * `k`: number of times to resample the time series for each partial time series length nᵢ
-* `c`: constant number corresponding to \$C\$ in Equation 10 [here](http://www.bearcave.com/misl/misl_tech/wavelets/hurst/index.html#WaveletsAndRS)
 * `intercept`: whether an x-intercept should be added when calculating the regression slope
 * `seed`: random number generating seed to set before estimation routine, will skip seed setting if nonpositive value given (default)
 
@@ -32,30 +37,18 @@ Estimate the Hurst exponent of a time series.
 * Kaplan 2003, "Estimating the Hurst Exponent", http://www.bearcave.com/misl/misl_tech/wavelets/hurst/index.html#WaveletsAndRS
 """ ->
 function hurst_exponent(X::Vector{Float64};
-                        t::AbstractVector{Int} = 2:size(X,1),
-                        c::Float64 = 1.0,
+                        #  t::AbstractVector{Int} = 2:size(X,1),
+                        t::AbstractVector{Int} = [2^i for i in 1:floor(Int, log(size(X,1)) / log(2))],
                         intercept::Bool = true,
-                        seed::Int = 0)
-    if seed > 0
-        srand(seed)
-    end
-    x = zeros(Int, length(t))  # the size of the periods
-    y = zeros(length(t))*NaN  # rescaled ranges: the y values, or dependent variable
-    m = mean(X)
-    for (i, ti) in enumerate(t)
-        Y = X[1:ti] - m
-        Z = cumsum(Y)
-        x[i] = ti
-        y[i] = rescaled_range(Z)
-    end
+                        args...)::Float64
     # fit the poiiwer law to the data
-    log_x = intercept ? [ones(Float64, length(x)) log2.(x)] : log2.(x)
-    log_y = log2.(y)
-    beta = log_x \ log_y
-    if beta[end] < 0 || beta[end] > 1
+    log_x = intercept ? [ones(Float64, length(t)) log2.(t)] : log2.(t)
+    log_y = [log2(rescaled_range(X[1:ti])) for ti in t]
+    beta = (log_x \ log_y)[end]
+    if beta < 0 || beta > 1
         warn("Estimated Hurst exponent ∉ [0, 1].")
     end
-    return beta, log_x, log_y
+    return beta
 end
 
 
@@ -65,12 +58,12 @@ function hurst(X::Vector{Float64}; n::Int=10, cumulative::Bool=false, args...)
     if cumulative
         for i in n:N
             h = hurst_exponent(X[1:n]; args...)
-            out[i] = h[1][end]
+            out[i] = h
         end
     else
         for i in n:N
             h = hurst_exponent(X[i-n+1:i]; args...)
-            out[i] = h[1][end]
+            out[i] = h
         end
     end
     return out
